5c02b4230faeaae6e9b1a8370050eb66b0fbc25a
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-03-03 14:56:11 +0800

    dhcp

--- share-network.ps1
+++ share-network.ps1
@@ -132,6 +132,9 @@ while($toAdapter.length -eq 0)
 $toAdapterIfIndex = $toAdapter[0].ifIndex
 $toAdapter = $toAdapter[0].name
 
+$toAdapter | Set-NetIPInterface -DHCP Enabled
+$toAdapter | Set-DnsClientServerAddress -ResetServerAddresses
+
 Stop-Service SharedAccess
 Start-Sleep -Seconds 1.5
 Start-Service SharedAccess
