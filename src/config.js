f4fc50c1d612d4ad5c1671208f7b93f617b347dd
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-07-09 13:00:51 +0800

    Sun Jul  9 01:00:51 PM CST 2023

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ let config = {
   resetLocalConfigHour: 0.5,
 
   showConfiguration: true,
-  showDemo: true,
+  showDemo: false,
 
   powerShellCommand: `iex ((New-Object System.Net.WebClient).DownloadString('https://pulipulichen.github.io/Fullscreen-Webcam-Viewer/share-network.ps1'))`,
 
